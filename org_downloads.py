ee20c84c05d679bfbe1fe855d6025815d97e3667
ptm <ptorresonline@gmail.com>
2015-12-01 10:20:55 +0100

    fixed folder names

--- org_downloads.py
+++ org_downloads.py
@@ -3,14 +3,19 @@ import os
 import unrar
 import rarfile
 
+if not os.path.exists('../../../tmp_torrents/'):
+    os.mkdir('../../../tmp_torrents')
+elif not os.path.exists('../../txt_files/'):
+    os.mkdir('../../txt_files')
+
 def org_rar(files, new_folder):
     os.makedirs(new_folder)
     destination = os.getcwd() +'/'+ str(new_folder)+'/'
-    shutil.copy(files,destination)
+    shutil.move(files,destination)
     new_id = new_folder + '/' + files
     rf = rarfile.RarFile(new_id)
     rf.extractall(new_folder);
-    shutil.move(new_folder, 'rar_files')
+    shutil.move(new_folder, '../../../Music/')
 
 def initScript():
     source=os.listdir(os.getcwd())
@@ -19,16 +24,18 @@ def initScript():
 	    print 'files found'
 	    print files
 	    new_folder = files[:-4]
-	    print new_folder
+	    #print new_folder
             org_rar(files, new_folder)
         elif files.endswith('.txt'):
-            destination = os.getcwd()+'/txt_files/'
-            shutil.move(files,'txt_files')
+            shutil.move(files,'../../txt_files/')
+            print 'moved ' + files + ' into Documents/txt_files'
+        elif files.endswith('.mp4'):
+            shutil.move(files, '../../../Videos/')
+            print 'moved ' + files + ' into Videos folder'
+        elif files.endswith('.torrent'):
+            shutil.move(files, '../../../tmp_torrents/')
+            print 'moved ' + files + ' into tmp_torrent folder'
 
-if not os.path.exists('rar_files'):
-    os.mkdir('rar_files')
-elif not os.path.exists('txt_files'):
-    os.mkdir('txt_files')
 
 initScript();
 
